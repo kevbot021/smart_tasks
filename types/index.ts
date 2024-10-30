bc27f6e2a6fc28161fd0ed7ea0d0f2546fabbbe8
kevbot021 <hello@kevindunbar.design>
2024-10-30 15:53:41 +0200

    Consolidating types/index into one folder

--- types/index.ts
+++ types/index.ts
@@ -13,20 +13,25 @@ export interface Task {
   id: string;
   description: string;
   is_complete: boolean;
-  category: string;
-  assigned_user_id: string | null;
+  category?: string;
+  assigned_user_id?: string;
+  created_by_user_id?: string;
   team_id: string;
   created_at: string;
-  created_by_user_id: string;
   audio_summary?: string;
   image?: string;
-  sub_tasks?: Subtask[];
   cartoon_slides?: string;
   assigner?: {
     id: string;
     name: string;
     email: string;
   };
+  assigned_user?: {
+    id: string;
+    name: string;
+    email: string;
+  };
+  sub_tasks?: Subtask[];
 }
 
 export interface Subtask {
