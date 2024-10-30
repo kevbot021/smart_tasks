28206f972407e3ba5a1f5d4c7e4a7bb64d799ba1
kevbot021 <hello@kevindunbar.design>
2024-10-30 14:51:07 +0200

    Fix vercel error

--- app/types/index.ts
+++ app/types/index.ts
@@ -7,6 +7,16 @@ export interface Task {
   created_by_user_id?: string;
   team_id: string;
   created_at: string;
+  assigner?: {
+    id: string;
+    name: string;
+    email: string;
+  };
+  assigned_user?: {
+    id: string;
+    name: string;
+    email: string;
+  };
   sub_tasks?: {
     id: string;
     description: string;
